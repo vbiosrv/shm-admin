40eb34bc1663886ca4dd93111aa360c23b333ed4
vbiosrv <fr0m.smef@gmail.com>
2026-01-03 17:03:32 +0300

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -575,7 +575,7 @@ function Layout() {
             })}
           </nav>
             <a
-              href="https://myshm.ru"
+              href="https://vbios.ru"
               target="_blank"
               rel="noopener noreferrer"
               className="px-2 py-2"
@@ -584,7 +584,7 @@ function Layout() {
               }}
               title="Telegram"
             >
-              MySHM.ru
+              vBios Service
             </a>
         </div>
       </aside>
@@ -611,7 +611,7 @@ function Layout() {
           </div>
           <div className="flex items-center gap-2">
             <a
-              href="https://t.me/shm_billing"
+              href="https://t.me/mrzbill_bot"
               target="_blank"
               rel="noopener noreferrer"
               className="flex items-center gap-1 px-2 py-1.5 rounded-lg text-sm transition-all"
@@ -622,34 +622,6 @@ function Layout() {
             >
               <Send className="w-5 h-5" />
             </a>
-            {version !== null && (
-              <a
-                href="https://github.com/danuk/shm"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="flex items-center gap-1 px-2 py-1.5 rounded-lg text-sm transition-all"
-                style={{
-                  color: 'var(--theme-content-text-muted)'
-                }}
-              >
-                <span>{version}</span>
-              </a>
-            )}
-            {stars !== null && (
-              <a
-                href="https://github.com/danuk/shm"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="flex items-center gap-1 px-2 py-1.5 rounded-lg text-sm transition-all"
-                style={{
-                  color: 'var(--theme-content-text-muted)'
-                }}
-              >
-                <Github className="w-5 h-5" />
-                <span>{stars}</span>
-                <Star className="w-5 h-5" />
-              </a>
-            )}
             <ThemeToggle />
             <button
               onClick={handleLogout}
